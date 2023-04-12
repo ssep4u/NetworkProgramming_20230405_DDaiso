856dc2517e471826938925ddbdc9babaadda493e
시아아빠 <ssep4u@gmail.com>
2023-04-12 09:49:10 +0900

    urls product:list

---
+++ product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from product import views
+
+app_name = 'product'
+
+urlpatterns = [
+    path('list/', views.ProductListView.as_view(), name='list'),    #product:list
+]
