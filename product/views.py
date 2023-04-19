f4b7a2df3c025c78e4374f3fd47e788c76c716d8
시아아빠 <ssep4u@gmail.com>
2023-04-19 10:00:57 +0900

    views ProductDeleteView

--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from product.models import Product
 
@@ -26,5 +26,11 @@ class ProductUpdateView(UpdateView):
     model = Product
     fields = ['name', 'price']  # '__all__'
     template_name_suffix = '_update'  # product_form.html -> product_update.html
-    #일반적으로 성공하면 detail로 간다
-    #success_url = reverse_lazy('product:list')  # 수정 성공하면, 이동할 url 이름
+    # 일반적으로 성공하면 detail로 간다
+    # success_url = reverse_lazy('product:list')  # 수정 성공하면, 이동할 url 이름
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('product:list')  # 삭제 성공하면, 이동할 url 이름
+    # product_confirm_delete.html
