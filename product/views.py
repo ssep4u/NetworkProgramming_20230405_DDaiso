69f357177fea4e65bb320d6229434aca64c75c0f
시아아빠 <ssep4u@gmail.com>
2023-06-07 09:15:41 +0900

    FBV update_product

--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from product.forms import ProductCreationForm
+from product.forms import ProductCreationForm, ProductChangeForm
 from product.models import Product
 
 
@@ -37,15 +37,14 @@ class ProductCreateView(CreateView):
 
 
 def create_product(request):
-    if request.method == 'POST': #사용자가 입력하고 submit 버튼 눌렀을 때
+    if request.method == 'POST':  # 사용자가 입력하고 submit 버튼 눌렀을 때
         form = ProductCreationForm(request.POST)
-        if form.is_valid():    #form을 검사하자
-            form.save() #form에 있는 정보를 저장하자
-        return redirect('product:list2')    #저장하고 product:list2로 넘어가자
-    else:   #처음에 빈 폼 보여주자
+        if form.is_valid():  # form을 검사하자
+            form.save()  # form에 있는 정보를 저장하자
+        return redirect('product:list2')  # 저장하고 product:list2로 넘어가자
+    else:  # 처음에 빈 폼 보여주자
         form = ProductCreationForm()
     return render(request, 'product/product_create.html', {'form': form})
-    
 
 
 class ProductUpdateView(UpdateView):
@@ -54,6 +53,21 @@ class ProductUpdateView(UpdateView):
     template_name_suffix = '_update'  # product_form.html -> product_update.html
     # 일반적으로 성공하면 detail로 간다
     # success_url = reverse_lazy('product:list')  # 수정 성공하면, 이동할 url 이름
+
+
+def update_product(request, pk):
+    if request.method == 'POST':  # 사용자가 입력하고 submit 버튼 눌렀을 때
+        form = ProductChangeForm(request.POST)  # form에 있는 내용 가져오자
+        if form.is_valid():  # form 검사하자
+            seleted_product = Product.objects.get(pk=pk)  # pk로 Product에서 하나 꺼내자
+            seleted_product.name = form.cleaned_data.get('name')  # 입력한 내용으로 product 수정하자
+            seleted_product.price = form.cleaned_data.get('price')
+            seleted_product.save()  # product 저장하자
+        return redirect('product:detail2', pk=pk)
+    else:  # 처음에 선택한 내용을 폼으로 보여주자
+        seleted_product = Product.objects.get(pk=pk)  # pk로 Product에서 하나 꺼내자
+        form = ProductChangeForm(instance=seleted_product)  # form에 표시하자
+    return render(request, 'product/product_update.html', {'form': form})
 
 
 class ProductDeleteView(DeleteView):
